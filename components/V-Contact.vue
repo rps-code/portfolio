38a5a537f91b43a647f5041ff99c04309377e711
rps <71778887+rps-code@users.noreply.github.com>
2025-08-01 18:44:02 +0100

    refactor(V-Contact, V-Menu): remove deprecated functions

--- components/V-Contact.vue
+++ components/V-Contact.vue
@@ -1,44 +1,41 @@
 <script setup>
-const { $smoothScrollBreakPoint } = useNuxtApp();
-const { gsap } = useGsap();
+  const { $smoothScrollBreakPoint } = useNuxtApp()
+  const { gsap } = useGsap()
 
-const rowsCount = 3;
-const textCount = 4;
+  const rowsCount = 3
+  const textCount = 4
 
-const contact = ref(null);
-const contactEmailRow = ref(null);
+  const contact = ref(null)
+  const contactEmailRow = ref(null)
 
-onMounted(() => {
-  const animations = [];
+  onMounted(() => {
+    const animations = []
 
-  const scrollTriggerFactory = (trigger) => ({
-    trigger,
-    scrub: window.innerWidth >= $smoothScrollBreakPoint ? true : 0.5,
-  });
+    const scrollTriggerFactory = trigger => ({
+      trigger,
+      scrub: window.innerWidth >= $smoothScrollBreakPoint ? true : 0.5
+    })
 
-  contactEmailRow.value.forEach((item) => {
-    const scrollTrigger = scrollTriggerFactory(item);
+    contactEmailRow.value.forEach(item => {
+      const scrollTrigger = scrollTriggerFactory(item)
 
-    const sumWidth =
-      item.offsetWidth + window.innerWidth / (window.innerWidth < 700 ? 2 : 4);
+      const sumWidth = item.offsetWidth + window.innerWidth / (window.innerWidth < 700 ? 2 : 4)
 
-    animations.push(
-      gsap.fromTo(
-        item,
-        { x: window.innerWidth },
-        { x: -sumWidth, scrollTrigger },
-      ),
-    );
-  });
+      animations.push(gsap.fromTo(item, { x: window.innerWidth }, { x: -sumWidth, scrollTrigger }))
+    })
 
-  onBeforeUnmount(() => {
-    animations.forEach((animation) => animation.scrollTrigger.kill());
-  });
-});
+    onBeforeUnmount(() => {
+      animations.forEach(animation => animation.scrollTrigger.kill())
+    })
+  })
 </script>
 
 <template>
-  <section ref="contact" class="contact" data-scroll-section>
+  <section
+    ref="contact"
+    class="contact"
+    data-scroll-section
+  >
     <NuxtLink
       ref="contactEmail"
       v-hoverable.mail
@@ -58,8 +55,7 @@ onMounted(() => {
           aria-hidden="true"
           class="contact__email__row__text"
         >
-          Send me an <span class="serif">email</span
-          ><span v-if="textKey !== textCount">&nbsp; - &nbsp;</span>
+          Send me an <span class="serif">email</span><span v-if="textKey !== textCount">&nbsp; - &nbsp;</span>
         </span>
       </span>
     </NuxtLink>
@@ -67,53 +63,51 @@ onMounted(() => {
 </template>
 
 <style lang="scss">
-.contact {
-  width: 100%;
+  @use 'sass:color';
 
-  padding: 5rem 0 8rem;
-  margin-top: -3px;
+  .contact {
+    width: 100%;
 
-  border-radius: 0 0 2rem 2rem;
-  background-color: var(--surface-color);
+    padding: 5rem 0 8rem;
+    margin-top: -3px;
 
-  overflow: hidden;
-  pointer-events: all;
+    border-radius: 0 0 2rem 2rem;
+    background-color: var(--surface-color);
 
-  box-shadow: 0 2rem 1.125rem -1.125rem rgba($color: #ebebeb, $alpha: 0.0875);
+    overflow: hidden;
+    pointer-events: all;
 
-  &__email {
-    display: block;
+    box-shadow: 0 2rem 1.125rem -1.125rem rgba($color: #ebebeb, $alpha: 0.0875);
 
-    position: relative;
-
-    color: darken($color: white, $amount: 40);
-    line-height: 1;
-    text-decoration: none;
-
-    cursor: none;
-
-    &__row {
+    &__email {
       display: block;
+      position: relative;
 
-      font-size: var(--step-5);
-      margin: 0;
+      color: color.adjust(white, $lightness: -40%);
+      line-height: 1;
+      text-decoration: none;
+      cursor: none;
 
-      white-space: nowrap;
-      will-change: transform;
+      &__row {
+        display: block;
+        font-size: var(--step-5);
+        margin: 0;
+        white-space: nowrap;
+        will-change: transform;
 
-      @supports (-webkit-text-stroke: 1px white) {
-        color: transparent;
-        -webkit-text-stroke: 1px darken($color: white, $amount: 40);
+        @supports (-webkit-text-stroke: 1px white) {
+          color: transparent;
+          -webkit-text-stroke: 1px color.adjust(white, $lightness: -40%);
+        }
+      }
+
+      @media (prefers-reduced-motion: reduce) {
+        cursor: pointer;
       }
     }
 
-    @media (prefers-reduced-motion: reduce) {
-      cursor: pointer;
+    @media (prefers-color-scheme: light) {
+      box-shadow: 0 2rem 0.75rem -1rem rgba($color: #030303, $alpha: 0.025);
     }
   }
-
-  @media (prefers-color-scheme: light) {
-    box-shadow: 0 2rem 0.75rem -1rem rgba($color: #030303, $alpha: 0.025);
-  }
-}
 </style>
--- components/V-Menu.vue
+++ components/V-Menu.vue
@@ -1,144 +1,138 @@
 <script setup>
-import { socialLinks } from '~/lib/constants';
+  import { socialLinks } from '~/lib/constants'
 
-const { $smoothScroll } = useNuxtApp();
-const isDarkMode = useDarkMode();
+  const { $smoothScroll } = useNuxtApp()
+  const isDarkMode = useDarkMode()
 
-const { gsap } = useGsap();
-const currentSection = useCurrentSection();
-const isShowingMenu = useMenuToggle();
-const prefersReducedMotion = useReducedMotion();
+  const { gsap } = useGsap()
+  const currentSection = useCurrentSection()
+  const isShowingMenu = useMenuToggle()
+  const prefersReducedMotion = useReducedMotion()
 
-const navigationalLinks = [
-  {
-    label: 'Home',
-    action: () => {
-      $smoothScroll.scrollTo(0);
-      isShowingMenu.value = false;
+  const navigationalLinks = [
+    {
+      label: 'Home',
+      action: () => {
+        $smoothScroll.scrollTo(0)
+        isShowingMenu.value = false
+      }
     },
-  },
-  {
-    label: 'Projects',
-    action: () => {
-      $smoothScroll.scrollTo('.projects');
-      isShowingMenu.value = false;
+    {
+      label: 'Projects',
+      action: () => {
+        $smoothScroll.scrollTo('.projects')
+        isShowingMenu.value = false
+      }
     },
-  },
-  {
-    label: 'About',
-    action: () => {
-      $smoothScroll.scrollTo('.about-me');
-      isShowingMenu.value = false;
+    {
+      label: 'About',
+      action: () => {
+        $smoothScroll.scrollTo('.about-me')
+        isShowingMenu.value = false
+      }
     },
-  },
-  {
-    label: 'Contact',
-    action: () => {
-      $smoothScroll.scrollTo('.contact');
-      isShowingMenu.value = false;
-    },
-  },
-];
+    {
+      label: 'Contact',
+      action: () => {
+        $smoothScroll.scrollTo('.contact')
+        isShowingMenu.value = false
+      }
+    }
+  ]
 
-const menu = ref(null);
-const menuBackItem = ref([]);
-const menuBackItemLine = ref([]);
-const menuBackItemContentTitle = ref([]);
-const menuBackItemContentLinksItem = ref([]);
+  const menu = ref(null)
+  const menuBackItem = ref([])
+  const menuBackItemLine = ref([])
+  const menuBackItemContentTitle = ref([])
+  const menuBackItemContentLinksItem = ref([])
 
-let prevAnimation;
+  let prevAnimation
 
-function showMenu() {
-  if (prevAnimation) prevAnimation.kill();
+  function showMenu() {
+    if (prevAnimation) prevAnimation.kill()
 
-  if (prefersReducedMotion.value)
-    return (prevAnimation = gsap.fromTo(
+    if (prefersReducedMotion.value) return (prevAnimation = gsap.fromTo(menu.value, { autoAlpha: 0 }, { autoAlpha: 1 }))
+
+    prevAnimation = gsap.timeline({
+      defaults: { ease: 'expo.out', duration: 1 }
+    })
+
+    prevAnimation.set(menu.value, {
+      autoAlpha: 1
+    })
+
+    prevAnimation.fromTo(
+      menuBackItem.value,
+      {
+        yPercent: 100,
+        rotateX: -90,
+        opacity: 0,
+        transformOrigin: 'center top',
+        transformStyle: 'preserve-3d'
+      },
+      {
+        yPercent: 0,
+        rotateX: 0,
+        opacity: 1,
+        stagger: 0.05
+      }
+    )
+    prevAnimation.fromTo(
       menu.value,
-      { autoAlpha: 0 },
-      { autoAlpha: 1 },
-    ));
-
-  prevAnimation = gsap.timeline({
-    defaults: { ease: 'expo.out', duration: 1 },
-  });
-
-  prevAnimation.set(menu.value, {
-    autoAlpha: 1,
-  });
-
-  prevAnimation.fromTo(
-    menuBackItem.value,
-    {
-      yPercent: 100,
-      rotateX: -90,
-      opacity: 0,
-      transformOrigin: 'center top',
-      transformStyle: 'preserve-3d',
-    },
-    {
-      yPercent: 0,
-      rotateX: 0,
-      opacity: 1,
-      stagger: 0.05,
-    },
-  );
-  prevAnimation.fromTo(
-    menu.value,
-    { backgroundColor: 'transparent' },
-    {
-      backgroundColor: isDarkMode.value ? '#030303' : '#f7f7f7',
-      duration: 0.2,
-    },
-    '-=0.65',
-  );
-  prevAnimation.fromTo(
-    menuBackItemLine.value,
-    { transform: 'scaleX(0)', transformOrigin: 'left bottom' },
-    { transform: 'scaleX(1)', stagger: 0.05 },
-    '<-0.2',
-  );
-  prevAnimation.fromTo(
-    [...menuBackItemContentTitle.value, ...menuBackItemContentLinksItem.value],
-    { opacity: 0, yPercent: 50 },
-    {
-      opacity: 1,
-      yPercent: 0,
-      stagger: 0.05,
-    },
-    '<-0.1',
-  );
-}
-
-function hideMenu() {
-  if (prevAnimation) prevAnimation.kill();
-
-  prevAnimation = gsap.to(menu.value, {
-    autoAlpha: 0,
-  });
-}
-
-watch(isShowingMenu, (bool) => {
-  if (bool) {
-    $smoothScroll.disable();
-    showMenu();
-  } else {
-    $smoothScroll.enable();
-    hideMenu();
+      { backgroundColor: 'transparent' },
+      {
+        backgroundColor: isDarkMode.value ? '#030303' : '#f7f7f7',
+        duration: 0.2
+      },
+      '-=0.65'
+    )
+    prevAnimation.fromTo(menuBackItemLine.value, { transform: 'scaleX(0)', transformOrigin: 'left bottom' }, { transform: 'scaleX(1)', stagger: 0.05 }, '<-0.2')
+    prevAnimation.fromTo(
+      [...menuBackItemContentTitle.value, ...menuBackItemContentLinksItem.value],
+      { opacity: 0, yPercent: 50 },
+      {
+        opacity: 1,
+        yPercent: 0,
+        stagger: 0.05
+      },
+      '<-0.1'
+    )
   }
-});
+
+  function hideMenu() {
+    if (prevAnimation) prevAnimation.kill()
+
+    prevAnimation = gsap.to(menu.value, {
+      autoAlpha: 0
+    })
+  }
+
+  watch(isShowingMenu, bool => {
+    if (bool) {
+      $smoothScroll.disable()
+      showMenu()
+    } else {
+      $smoothScroll.enable()
+      hideMenu()
+    }
+  })
 </script>
 
 <template>
-  <div ref="menu" class="menu" role="menu">
+  <div
+    ref="menu"
+    class="menu"
+    role="menu"
+  >
     <div
-      :ref="(el) => (menuBackItem[0] = el)"
+      :ref="el => (menuBackItem[0] = el)"
       class="menu__back-item"
       aria-hidden="true"
     ></div>
     <div
       v-for="(link, key) in navigationalLinks"
       :key="key"
-      :ref="(el) => (menuBackItem[key + 1] = el)"
+      :ref="el => (menuBackItem[key + 1] = el)"
       class="menu__back-item"
       role="menuitem"
     >
@@ -146,34 +140,40 @@ watch(isShowingMenu, (bool) => {
         role="button"
         :class="{
           'menu__back-item__content': true,
-          'menu__back-item__content--active': key === currentSection,
+          'menu__back-item__content--active': key === currentSection
         }"
         tabindex="0"
         :aria-label="`scroll to ${link.label}`"
         @click="link.action"
       >
         <p
-          :ref="(el) => (menuBackItemContentTitle[key] = el)"
+          :ref="el => (menuBackItemContentTitle[key] = el)"
           class="menu__back-item__content__title"
         >
           <span>{{ link.label }}</span>
         </p>
       </div>
       <span
-        :ref="(el) => (menuBackItemLine[key] = el)"
+        :ref="el => (menuBackItemLine[key] = el)"
         class="menu__back-item__line"
       ></span>
     </div>
-    <div :ref="(el) => (menuBackItem[5] = el)" class="menu__back-item">
+    <div
+      :ref="el => (menuBackItem[5] = el)"
+      class="menu__back-item"
+    >
       <div class="menu__back-item__content menu__back-item__content--no-anim">
         <ul class="menu__back-item__content__links">
           <li
             v-for="(link, key) in socialLinks"
             :key="key"
-            :ref="(el) => (menuBackItemContentLinksItem[key] = el)"
+            :ref="el => (menuBackItemContentLinksItem[key] = el)"
             class="menu__back-item__content__links__item"
           >
-            <NuxtLink :href="link.href" target="_blank">
+            <NuxtLink
+              :href="link.href"
+              target="_blank"
+            >
               {{ link.label }}
             </NuxtLink>
           </li>
@@ -184,163 +184,165 @@ watch(isShowingMenu, (bool) => {
 </template>
 
 <style lang="scss">
-.menu {
-  --100vh: calc(100 * var(--vh, 1vh));
+  @use 'sass:color';
 
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 6;
+  .menu {
+    --100vh: calc(100 * var(--vh, 1vh));
 
-  width: 100%;
-  height: 100%;
-  height: var(--100vh);
-
-  opacity: 0;
-  visibility: hidden;
-  pointer-events: all;
-
-  &__back-item {
-    $ITEMS_COUNT: 6;
-    --ease-back: cubic-bezier(0.34, 1.56, 0.64, 1);
-    --x-padding: calc(clamp(1rem, 4vw, 5rem) + var(--step-0));
-
-    position: absolute;
+    position: fixed;
+    top: 0;
     left: 0;
-    right: 0;
+    z-index: 6;
 
-    height: calc(var(--100vh) / #{$ITEMS_COUNT});
+    width: 100%;
+    height: 100%;
+    height: var(--100vh);
 
-    padding: 1rem var(--x-padding);
-    background-color: var(--surface-color);
+    opacity: 0;
+    visibility: hidden;
+    pointer-events: all;
 
-    @for $i from 0 to $ITEMS_COUNT {
-      &:nth-child(#{$i + 1}) {
-        top: calc(#{$i} * calc(var(--100vh) / #{$ITEMS_COUNT}));
-      }
-    }
+    &__back-item {
+      $ITEMS_COUNT: 6;
+      --ease-back: cubic-bezier(0.34, 1.56, 0.64, 1);
+      --x-padding: calc(clamp(1rem, 4vw, 5rem) + var(--step-0));
 
-    &__content {
-      display: flex;
-      justify-content: flex-start;
-      align-items: flex-end;
+      position: absolute;
+      left: 0;
+      right: 0;
 
-      height: 100%;
+      height: calc(var(--100vh) / #{$ITEMS_COUNT});
 
-      cursor: pointer;
+      padding: 1rem var(--x-padding);
+      background-color: var(--surface-color);
 
-      &__title {
-        position: relative;
-
-        color: darken($color: #fff, $amount: 50);
-        font-size: var(--step-3);
-        line-height: 1;
-
-        width: 100%;
-        margin: 0;
-
-        span {
-          display: inline-block;
-          transform-origin: left center;
-          transition:
-            color 200ms,
-            transform 200ms var(--ease-back);
-        }
-
-        &::after {
-          --size: var(--step--1);
-
-          content: '';
-          position: absolute;
-          top: 50%;
-          right: 0;
-
-          width: var(--size);
-          height: var(--size);
-          border-radius: 50%;
-          background-color: #ffe6ed;
-          opacity: 0;
-
-          transform: translate(-50%, -50%);
-          transition: opacity 200ms ease;
-
-          @media (prefers-color-scheme: light) {
-            background-color: #303030;
-          }
+      @for $i from 0 to $ITEMS_COUNT {
+        &:nth-child(#{$i + 1}) {
+          top: calc(#{$i} * calc(var(--100vh) / #{$ITEMS_COUNT}));
         }
       }
 
-      &__links {
+      &__content {
         display: flex;
         justify-content: flex-start;
-        align-items: center;
-        gap: var(--step-1);
+        align-items: flex-end;
 
-        list-style-type: none;
-        padding-inline-start: 0;
+        height: 100%;
 
-        &__item a {
-          font-size: calc(var(--step--2) - 0.075rem);
-          color: darken($color: #fff, $amount: 50);
-          text-transform: uppercase;
-          text-decoration: none;
+        cursor: pointer;
 
-          transition: color 100ms;
+        &__title {
+          position: relative;
 
-          &:is(:hover, :focus) {
-            color: #ffe6ed;
+          color: color.adjust(#fff, $lightness: -50%);
+          font-size: var(--step-3);
+          line-height: 1;
+
+          width: 100%;
+          margin: 0;
+
+          span {
+            display: inline-block;
+            transform-origin: left center;
+            transition:
+              color 200ms,
+              transform 200ms var(--ease-back);
+          }
+
+          &::after {
+            --size: var(--step--1);
+
+            content: '';
+            position: absolute;
+            top: 50%;
+            right: 0;
+
+            width: var(--size);
+            height: var(--size);
+            border-radius: 50%;
+            background-color: #ffe6ed;
+            opacity: 0;
+
+            transform: translate(-50%, -50%);
+            transition: opacity 200ms ease;
 
             @media (prefers-color-scheme: light) {
-              color: #030303;
+              background-color: #303030;
             }
           }
         }
-      }
 
-      &:is(:focus, :hover) .menu__back-item__content__title {
-        color: #ffe6ed;
+        &__links {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          gap: var(--step-1);
 
-        span {
-          transform: scale(0.96);
+          list-style-type: none;
+          padding-inline-start: 0;
+
+          &__item a {
+            font-size: calc(var(--step--2) - 0.075rem);
+            color: color.adjust(#fff, $lightness: -50%);
+            text-transform: uppercase;
+            text-decoration: none;
+
+            transition: color 100ms;
+
+            &:is(:hover, :focus) {
+              color: #ffe6ed;
+
+              @media (prefers-color-scheme: light) {
+                color: #030303;
+              }
+            }
+          }
         }
 
-        @media (prefers-color-scheme: light) {
-          color: #030303;
-        }
-      }
-
-      &--active {
-        .menu__back-item__content__title {
+        &:is(:focus, :hover) .menu__back-item__content__title {
           color: #ffe6ed;
 
-          &::after {
-            opacity: 1;
+          span {
+            transform: scale(0.96);
           }
 
           @media (prefers-color-scheme: light) {
             color: #030303;
           }
         }
-      }
 
-      &--no-anim {
-        &:is(:hover, :focus) {
-          transform: none;
+        &--active {
+          .menu__back-item__content__title {
+            color: #ffe6ed;
+
+            &::after {
+              opacity: 1;
+            }
+
+            @media (prefers-color-scheme: light) {
+              color: #030303;
+            }
+          }
+        }
+
+        &--no-anim {
+          &:is(:hover, :focus) {
+            transform: none;
+          }
         }
       }
-    }
 
-    &__line {
-      display: inline-block;
+      &__line {
+        display: inline-block;
 
-      position: absolute;
-      bottom: 1%;
-      left: var(--x-padding);
-      right: var(--x-padding);
+        position: absolute;
+        bottom: 1%;
+        left: var(--x-padding);
+        right: var(--x-padding);
 
-      height: 1px;
-      background-color: darken($color: #fff, $amount: 75);
+        height: 1px;
+        background-color: color.adjust(#fff, $lightness: -75%);
+      }
     }
   }
-}
 </style>
